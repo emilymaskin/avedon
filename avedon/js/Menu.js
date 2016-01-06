fb1fa424ad2957987c474c34715cf1c2b9caa568
Emily Maskin <emily.maskin@gmail.com>
2016-01-06 18:57:33 -0500

    cleaning up menu js

--- avedon/js/Menu.js
+++ avedon/js/Menu.js
@@ -12,37 +12,39 @@ Avedon.Menu = Backbone.View.extend({
     this.$homeSlides = $(opts.homeSlides);
     this.$siteContainer = $(opts.siteContainer);
 
-    // Menu container hide on mouseleave
-    this.$el.mouseleave(function() {
-      _this.hideMenu();
-    });
-
     _.bindAll(this, 'render', 'hideMenu');
   },
 
   render: function() {
     var _this = this;
 
-    $(this.$menuToggle).fadeOut('fast', function() {
-      _this.$el.slideDown(400);
-      _this.$screen.fadeIn('slow');
+    this.$menuToggle.fadeOut('fast');
+    this.$screen.fadeIn('slow');
+
+    this.$el.slideDown(400).promise().done(function() {
+      $('#menu').on('mouseleave', function() {
+        _this.hideMenu();
+      });
     });
-    $('#screen').click(this.hideMenu);
+
+    this.$screen.click(this.hideMenu);
   },
 
   hideMenu: function() {
     var _this = this;
 
-    $('#menu').slideUp(400, function() {
-      _this.$menuToggle.fadeIn('fast');
-    });
-    _this.$screen.fadeOut('slow');
+    this.$menuToggle.fadeIn('fast');
+    this.$screen.fadeOut('slow');
+
+    $('#menu').off('mouseleave');
+    this.$el.slideUp(400);
   },
 
   showInfoScreen: function(e) {
     var category = $(e.currentTarget).data('category');
 
     this.hideMenu();
+
     new Avedon.InfoScreen({
       el: '#' + category + 'Container',
       siteContainer: this.$siteContainer,
@@ -51,26 +53,28 @@ Avedon.Menu = Backbone.View.extend({
     }).render();
   },
 
-  changeCategory: function(e) {
-    var _this = this;
-    var category = $(e.target).data('category');
-    var $categorySlides = $('.' + category + 'Slides');
-
-    this.hideMenu();
-
+  _updateActiveMenuItem: function($selected) {
     $('#menucontainer .album').removeClass('selected');
-    $(e.target).addClass('selected');
+    $selected.addClass('selected');
+  },
+
+  _updateSlides: function(category) {
+    var $categorySlides = $('.' + category + 'Slides');
 
     this.$homeSlides.cycle('pause');
 
-    $('.active').fadeOut(1000).promise().done(function() {
-      $('#' + category).fadeIn(1200, function() {
-        $('.category').removeClass('active');
-        $(this).addClass('active');
-      });
-      _this.$menuToggle.fadeIn(600);
+    $('.mainElement').fadeOut(1000).promise().done(function() {
+      $('#' + category).fadeIn(1200);
       $categorySlides.cycle();
       $categorySlides.cycle('goto', 0);
     });
+  },
+
+  changeCategory: function(e) {
+    var category = $(e.target).data('category');
+
+    this._updateActiveMenuItem($(e.target));
+    this._updateSlides(category);
+    this.hideMenu();
   }
 });
